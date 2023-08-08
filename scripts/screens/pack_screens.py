194e39e590f83ebb6b5c28d0699a80d6dd623d30
Vixinlove <140357210+Vixinlove@users.noreply.github.com>
2023-08-08 18:34:26 -0400

    Add files via upload

---
+++ scripts/screens/pack_screens.py
@@ -0,0 +1,12 @@
+import random
+
+import pygame
+import pygame_gui
+from scripts.screens.catlist_screens import pack_button
+from scripts.cat.cats import ages, ages_moons, moons
+
+# idk what the fuvk i'm doingg!!! #
+
+show = 15
+
+
